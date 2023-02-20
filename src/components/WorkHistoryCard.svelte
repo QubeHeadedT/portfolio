01e313eb3b10ecdfcf950106740ecb80dc4df53f
James Leech <j.leech@sonraianalytics.gmail.com>
2023-02-20 20:18:37 +0000

    Made basic routing and a component for displaying a job-history "card"

---
+++ src/components/WorkHistoryCard.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let title = 'Job title';
+    export let logo = '/images/working_logo.png';
+    export let highlights = ['Made stuff', 'Did things'];
+</script>
+
+<div>
+    <img src={logo} alt="logo">
+    <h1>{title}</h1>
+    <ul>
+        {#each highlights as highlight}
+            <li>
+                <h6>{highlight}</h6>
+            </li>
+        {/each}
+    </ul>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
+<script>
+    import WorkHistoryCard from "../components/WorkHistoryCard.svelte";
+    let sonrai_logo = '/images/sonrai_logo.jpeg';
+    let sonrai_highlights = ['Made', 'Did'];
+    let title = 'Chief butter-patter';
+</script>
+
 <h1>Welcome to my website!</h1>
+<WorkHistoryCard title={title} logo={sonrai_logo} highlights={sonrai_highlights}/>
